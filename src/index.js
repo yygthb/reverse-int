338684e08b80e89fce1aade9170cbe260cb5da78
Yura Ya <yura@revpush.cc>
2021-12-30 16:26:31 +0300

    feat: function that reverse digits of this number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+  const numToString = Math.abs(n).toString();
+  let reversedString = '';
+
+  for (let i = numToString.length - 1; i >= 0; i--) {
+    reversedString += numToString[i];
+  }
+
+  return reversedString;
+};
